459b6c01f3725af02257c7512e1ed2696b67c973
J0shua78 <jwkoh78@yahoo.com>
2025-06-05 12:28:40 +0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function downloadCV() {
+  window.open("documents/CV_MOHD_RAIHAN.doc", "_blank");
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
+
+function goBack() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
